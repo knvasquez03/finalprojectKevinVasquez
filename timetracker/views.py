df0e9df519b26603c897b3a1f1e7732e4858f591
kev <knvasquez@mail.fhsu.edu>
2026-05-03 13:23:53 -0500

    Updated views.py and added register view with automatic login after signup.

--- timetracker/views.py
+++ timetracker/views.py
@@ -3,14 +3,27 @@
 # Final Project
 
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .models import Client, TimeEntry
-from .forms import ClientForm, TimeEntryForm
+from .forms import ClientForm, TimeEntryForm, RegisterForm
 
 
 def home(request):
     return render(request, 'timetracker/home.html')
 
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/register.html', {'form': form})
 
 @login_required
 def dashboard(request):
