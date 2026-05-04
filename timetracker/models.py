40cd1fca9a4030a5635844688821bb4e40f7a77a
kev <knvasquez@mail.fhsu.edu>
2026-05-03 19:01:15 -0500

    fixed model order to define Client before TimeEntry.

--- timetracker/models.py
+++ timetracker/models.py
@@ -5,6 +5,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True)
+    hourly_rate = models.DecimalField(max_digits=8, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class TimeEntry(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
